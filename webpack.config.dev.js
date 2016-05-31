1e54ef77a3d78659b9037dea0006716245410af4
Travis CI <riku.rouvila@gmail.com>
2016-05-31 20:00:58 +0000

    Deploy to GitHub Pages: bc1ee39b7dd47c4855170674b6f1bb2ae965d2a1

---
+++ webpack.config.dev.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  // or devtool: 'eval' to debug issues with compiled output:
+  devtool: 'eval',
+  entry: [
+    // necessary for hot reloading with IE:
+    'eventsource-polyfill',
+    // listen to code updates emitted by hot middleware:
+    'webpack-hot-middleware/client',
+    // your code:
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.json$/,
+      loader: 'json'
+    },
+    {
+      test: /\.css$/,
+      loaders: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
+  }
+};
