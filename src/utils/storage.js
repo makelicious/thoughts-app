dc4f6447d97b80b4ac9e5b5dffc8ed032986db26
Riku Rouvila <riku.rouvila@gmail.com>
2016-06-02 19:10:55 +0300

    test deploy

--- src/utils/storage.js
+++
@@ -1,7 +0,0 @@
-export function saveThoughts(thoughts) {
-  window.localStorage.setItem('thoughts', JSON.stringify(thoughts));
-}
-
-export function getThoughts() {
-  return JSON.parse(window.localStorage.getItem('thoughts')) || [];
-}
