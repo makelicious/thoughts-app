8e1bd4cbeffbe1b7ee86f4d7fd83fc65c0ac1a77
Riku Rouvila <riku.rouvila@gmail.com>
2016-06-02 19:22:57 +0300

    deploy different branches to different directories

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,8 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  npm run build
+  TARGET=$TRAVIS_BRANCH npm run build
 }
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -28,20 +21,21 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf dist/**/* || exit 0
+(rm -rf dist/$TRAVIS_BRANCH/**/*) || exit 0
 
 # Run our compile script
 doCompile
 
+cp deploy_key.enc dist/
+
 # Now let's go have some fun with the cloned repo
 cd dist
+
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
+if [ "$TRAVIS_BRANCH" == "$SOURCE_BRANCH" ]; then
+  cp -r $TRAVIS_BRANCH/* .
 fi
 
 # Commit the "changes", i.e. the new version.
