abdfc97a35a1a5582187f65b32486fe689cfbe9a
Riku Rouvila <riku.rouvila@gmail.com>
2016-06-18 23:14:16 +0300

    fix checkbox regexp

---
+++ src/utils/tests/text.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { replaceNth } from '../text';
+import { CHECKBOX_REGEXP } from 'utils/thought';
+
+describe('Text utility', () => {
+
+  describe('replaceNth', () => {
+    it('replaces the nth occurence of pattern with replacer', () => {
+      expect(replaceNth(
+        '#foobar hello world []',
+        0,
+        CHECKBOX_REGEXP,
+        '[x]'
+      )).to.equal('#foobar hello world [x]');
+    });
+  });
+
+});
