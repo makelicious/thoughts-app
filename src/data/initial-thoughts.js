1888464f1b56cd6e4da35315047883599cba4b6a
Markus Laurila <markuslaurila@outlook.com>
2016-07-11 17:02:29 +0300

    fix typo in intro

--- src/data/initial-thoughts.js
+++ src/data/initial-thoughts.js
@@ -2,5 +2,5 @@ export default [
   '#welcome\n' +
   '**Write down all the things you wish to remember**\n' +
   'Ideahigh helps you to recall ideas and handle your flow of thought\n' +
-  'both in your day-to-day tasks and in life in general'
+  'both in your day-to-day tasks and life in general'
 ];
